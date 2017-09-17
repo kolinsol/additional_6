ca68d68a8f1127bf99f4243e268573f290a3da77
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2017-09-17 14:22:37 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function zeros(expression) {
-  // your solution
+module.exports = function zeros(expr) {
+  expr = expr.split("*");
+  if (expr.every(x => /\d+!!/.test(x))
+      && expr.every(x => Number(x.replace(/(\d+).*/, "$1")) % 2 !== 0)) return 0;
+  return expr.map(getZeroes).reduce((acc, cur) => acc + cur, 0);
 }
+
+function getZeroes(fact) {
+  let num = Number(fact.replace(/(\d+)!+/, "$1"));
+  let sign = fact.replace(/\d+(!+)/, "$1");
+  let zeroes = 0;
+  let factors = [];
+  if (sign === "!") factors = Array(num).fill(0).map((_, i) => i + 1);
+  else if (num % 2 === 0) {
+    factors = Array(num).fill(0).map((_, i) => {
+      if ((i + 1) % 2 === 0) return i + 1;
+      else return 0;
+    }).filter(x => x !== 0);
+  } else {
+    factors = Array(num).fill(0).map((_, i) => {
+      if ((i + 1) % 2 !== 0) return i + 1;
+      else return 0;
+    }).filter(x => x !== 0);
+  }
+  while(factors.length > 0) {
+    factors = factors.filter(x => x % 5 === 0);
+    zeroes += factors.length;
+    factors = factors.map(x => x / 5);
+  }
+  return zeroes;
+}
+
+
+console.log(getZeroes("100!"));
